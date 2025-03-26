03d508c40418609bfc370b7a06d12bc65a0edae4
Jimmykotter <jimmykotter@gmail.com>
2025-03-25 20:15:52 -0500

    added portfolio images and links

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,3 +1,18 @@
 export default function Portfolio() {
-    return <h1>Portfolio</h1>
+    return <><h1>Portfolio</h1>
+    <h2> Here are some recent projects i've worked on! </h2>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeKLqaHxUIsdYS5vObhslQGLF93KuCDsHx_W8wNRqHQvrXX5Q/viewform">
+    <link href="/SIAV LOGO.PNG" />
+  </a>
+  
+
+<a href="https://github.com/Jimmykotter/MooLah" target="_blank" rel="noopener noreferrer">
+    <img src="../src/assets/Moolah.jpg" alt="Moolah" style={{ width: '200px', height: 'auto' }} />
+    <h4>Moolah is a personal monthly budget calculator</h4>
+</a>
+<a href="https://github.com/NeOpoku/DevCoachPro" target="_blank" rel="noopener noreferrer">
+    <img src="../src/assets/DevCoachPro.png" alt="Moolah" style={{ width: '200px', height: 'auto' }} />
+    <h4>DevCoachPro is an AI powered web developer coach</h4>
+</a>
+</>
 }
