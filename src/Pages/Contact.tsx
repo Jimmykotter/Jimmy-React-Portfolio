6b87cab6086e9bf24a73d7c47a22e64045a313ec
Jimmykotter <jimmykotter@gmail.com>
2025-03-25 19:59:56 -0500

    added contact page with link to google form

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,3 +1,10 @@
 export default function Contact() {
-        <h1>Contact</h1>
+  return (
+    <div>
+      <h1>Contact</h1>
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSeKLqaHxUIsdYS5vObhslQGLF93KuCDsHx_W8wNRqHQvrXX5Q/viewform">
+        Open Google Form
+      </a>
+    </div>
+  );
 }
