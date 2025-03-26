1a7b9329e9ce16e09ba3cf67c6d7e3cdc53ce4ba
Jimmykotter <jimmykotter@gmail.com>
2025-03-25 19:37:05 -0500

    added about page content

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,6 +1,17 @@
 // import Navbar from "./Pages/Navbar";
 
 export default function About() {
-    return <h1>About Content</h1>
-    
+    return <> <h1>About</h1>
+
+    <p>Hey, I’m Jimmy — a web developer, problem solver, and founder of Stereo Image AV, where I provide audio-visual production and rentals for concerts, corporate events, and everything in between.
+
+<p>My journey into tech started with curiosity and a drive to create. I now build full-stack web applications using React, TypeScript, Node.js, and PostgreSQL. I enjoy crafting clean, functional interfaces and building back-end systems that run smoothly and scale well.</p>
+
+I’m especially interested in how AI and automation can simplify workflows, enhance user experiences, and make powerful tools more accessible.</p>
+
+<p>Outside of coding, I have a strong background in music, live sound, and DIY projects. I bring the same attention to detail and creativity from those fields into my development work.
+
+Outside of work, I’m a proud dad, guitarist, and gamer. I believe in consistency, curiosity, and building things that matter.
+
+Thanks for stopping by — I’d love to connect! </p></>
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,17 +1,27 @@
-import myPhoto from '../assets/myPhoto.jpg';
+import myPhoto from "../assets/myPhoto.jpg";
 
-export default function Home(){
+export default function Home() {
+  return (
+    <>
+      <img
+        src={myPhoto}
+        alt="Jimmy"
+        style={{ width: "200px", borderRadius: "50%" }}
+      />
+      <h1>Hi, I'm Jimmy.</h1>
 
-    return <>
-    <img src={myPhoto} alt="Jimmy" style={{ width: '200px', borderRadius: '50%' }} />
-    <h1>Hi, I'm Jimmy.</h1>
+      <p>
+        I'm a creative technologist & problem solver.</p>
         
-    <p>
-    I'm a creative technologist & problem solver I specialize in building thoughtful, user-focused solutions through modern web technologies. Whether it's front-end interfaces, full-stack applications, or immersive audio/visual experiences.
-    </p>
-    <p>
-      Welcome to my portfolio, take a look around and feel free to reach out!
-    </p>
-    </>
-}
+        <p>I specialize in building thoughtful, user-focused solutions through modern web technologies.</p>
 
+      <p>
+        Whether it's front-end interfaces, full-stack applications, or immersive audio/visual experiences.
+      </p>
+
+      <h2>
+        Welcome to my portfolio, take a look around and feel free to reach out!
+      </h2>
+    </>
+  );
+}
