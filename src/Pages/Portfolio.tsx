650f2d5f79589e7adcd9d483f16f97b3256b7448
Jimmykotter <jimmykotter@gmail.com>
2025-03-25 22:14:30 -0500

    fix photo bug with deployment

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -3,11 +3,11 @@ export default function Portfolio() {
     <h2> Here are some recent projects i've worked on! </h2>
 
 <a href="https://github.com/Jimmykotter/MooLah" target="_blank" rel="noopener noreferrer">
-    <img src="../src/assets/Moolah.jpg" alt="Moolah" style={{ width: '200px', height: 'auto' }} />
+    <img src="../src/assets/MooLah.jpg" alt="Moolah" style={{ width: '200px', height: 'auto' }} />
     <h4>Moolah is a personal monthly budget calculator</h4>
 </a>
 <a href="https://github.com/NeOpoku/DevCoachPro" target="_blank" rel="noopener noreferrer">
-    <img src="../src/assets/DevCoachPro.png" alt="Moolah" style={{ width: '200px', height: 'auto' }} />
+    <img src="../src/assets/DevCoachPro.png" alt="DevCoachPro" style={{ width: '200px', height: 'auto' }} />
     <h4>DevCoachPro is an AI powered web developer coach</h4>
 </a>
 </>
